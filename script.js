5772b0e713baaa59ce5fa9ec663c00e59878340c
rodrigo33 <rbarbosa.barbosa@gmail.com>
2021-01-07 13:49:38 -0300

    implementado cactus, dino, estilizacoes, ajustado as acoes do dino

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+
 //use e abuse do console log para saber se estou no caminho certo
 
 //capturando quando pressionado espaço
 function handleKeyUp(event){
     if(event.keyCode === 32){
-        //chamando a funcao
-        jump();
+        if(!isJumping){
+            //chamando a funcao
+            jump();
+        }        
     }
 }
 
 //Função para dino saltar
 function jump(){
-    let position = 0;
+    isJumping = true;
 
     //com esta funcao, executa o codigo dentro do bloco de maneira repetida
     let upInterval = setInterval(() => {
@@ -21,6 +27,7 @@ function jump(){
             let downInterval = setInterval(() => {
                 if(position <=0){
                     clearInterval(downInterval)
+                    isJumping = false;
                 }else{
                     position -= 20;
                     dino.style.bottom = position + 'px';
@@ -37,6 +44,37 @@ function jump(){
 
     }, 20);
 }
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    //console.log(randomTime);
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //Game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+        }else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 
 //capturando quando pressiono tecla
 document.addEventListener('keyup', handleKeyUp);
